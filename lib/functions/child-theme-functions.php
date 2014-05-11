b7f4785752556c0c111c2480664a71a0ddca7948
Tibor Paulsch <4tibor@gmail.com>
2014-05-11 07:56:29 +0200

    Code styling

--- lib/functions/child-theme-functions.php
+++ lib/functions/child-theme-functions.php
@@ -166,16 +166,14 @@ function tibs_column_class( $i ) {
 
 // remove unused layouts
 add_action('genesis_setup', 'bsg_remove_unused_layout_options', 15);
-
-// modify bootstrap classes based on genesis_site_layout
-add_filter('bsg-classes-to-add', 'bsg_modify_classes_based_on_template', 10, 3);
-
 function bsg_remove_unused_layout_options() {
     genesis_unregister_layout( 'content-sidebar-sidebar' );
     genesis_unregister_layout( 'sidebar-sidebar-content' );
     genesis_unregister_layout( 'sidebar-content-sidebar' );
 }
 
+// modify bootstrap classes based on genesis_site_layout
+add_filter('bsg-classes-to-add', 'bsg_modify_classes_based_on_template', 10, 3);
 function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
 
     $layout = genesis_site_layout();
